04e67d1329d2d0c829fba08599e7f790d602b335
lucas.serna.quinto@gmail.com <lucas.serna.quinto@gmail.com>
2020-07-08 17:48:28 -0300

    Sorting the resulting list

--- assign_08_04/assign0804.py
+++ assign_08_04/assign0804.py
@@ -10,9 +10,6 @@ for line in fileh:
     #differentWords.append(line.split())    #since we are acessing each string, this would make a list of a list(the words of each string
     #differentWords = differentWords + line.split() #this on the other hand, will get each possible element we split and add to the current list for every line we go through
     wordsFromString=line.split() 
-    for word in wordsFromString:
-        if word in differentWords:
-            continue
-        differentWords.append(word)
-
+    
+lista.sort()
 print(differentWords)
